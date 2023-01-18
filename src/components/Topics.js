6ac940d01fca54e17b33d907aed8d504c5bf4153
lalmastar <107454522+lalmastar@users.noreply.github.com>
2023-01-18 13:37:38 +0530

    Update Topics.js

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,19 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Topics = () => {
   return (
     <div id="topics-div">
       <h2>Topics</h2>
       <ul>
-        <li>
-          <h2 id="rendering">Rendering</h2>
-        </li>
-        <li>
-          <h2 id="components">Components</h2>
-        </li>
+        <Link to='/topics/rendering'>
+          <li>
+            <h2 id="rendering">Rendering</h2>
+          </li>
+        </Link>
+        <Link to='/topics/components'>
+          <li>
+            <h2 id="components">Components</h2>
+          </li>
+        </Link>
       </ul>
       <hr />
-      <button id="topics-back-button">Back</button>
+      <Link to="/">
+        <button id="topics-back-button">Back</button>
+      </Link>
     </div>
   );
 };
